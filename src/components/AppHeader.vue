7c7072a5eff0ccf19a1b9a69952950a80c98bab9
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2022-06-28 15:53:25 -0400

    Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+       Header
+    </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AppHeader />
+    <h1>Default Layout</h1>
+    <slot />
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
