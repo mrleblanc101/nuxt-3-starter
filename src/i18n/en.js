a46f8cbf948bafb259d9e13d660051ac9bdbacf9
Krystina Hamel <krystina.hamel@libeo.com>
2023-09-06 08:23:13 -0400

    [FIX] Ajustement pour le bug de nuxt/i18n

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,5 +1,7 @@
-import app from '@/en/app';
+// import app from '@/en/app';
 
 export default {
-    app,
+    app: {
+        header: 'Nuxt 3 - Kit de démarrage',
+    },
 };
--- src/i18n/en/app.js
+++ src/i18n/en/app.js
@@ -1,3 +1,5 @@
+// À utiliser lorsque le bug d'import de fichiers de @nuxtjs/i18n sera réglé
+// https://github.com/nuxt-modules/i18n/issues/2215
 export default {
     header: 'Nuxt 3 - Kit de démarrage',
 };
