97e5d5810cbb15d8d9e2ac148842f3e1bb611649
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2024-05-13 02:55:41 -0400

    [FEATURE] Ajout affichage numéro de version

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,22 @@
 <template>
-    <footer></footer>
+    <footer>
+        <template v-if="runtimeConfig.public.commitTag || runtimeConfig.public.commitShortSha"> Version: </template>
+        {{ runtimeConfig.public.commitTag }}
+        {{
+            runtimeConfig.public.commitTag
+                ? `(${runtimeConfig.public.commitShortSha})`
+                : runtimeConfig.public.commitShortSha
+        }}
+    </footer>
 </template>
 
-<script>
-export default {};
+<script setup>
+const runtimeConfig = useRuntimeConfig();
 </script>
+
+<style lang="postcss" scoped>
+footer {
+    padding: 10px 20px 30px;
+    text-align: center;
+}
+</style>
